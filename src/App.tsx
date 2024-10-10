58ba7b7238b366665a0ef965329450d15b3dcfb2
Alexey <pixell390@gmail.com>
2024-10-10 12:36:24 +0300

    initial

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react"
+
+function App() {
+  return (
+    <main className="flex flex-col items-center justify-center">
+      <h1 className="text-3xl font-bold">Vite + React</h1>
+    </main>
+  )
+}
+
+export default App
